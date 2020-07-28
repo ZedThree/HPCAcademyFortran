9881623f00eb47cba06e6bcf7ab8a4f94dd4f143
Peter Hill <zed.three@gmail.com>
2020-07-28 11:33:36 +0100

    Remove some whitespace in example to fit lines on slide

--- examples/28_loss_of_precision.f90
+++ examples/28_loss_of_precision.f90
@@ -2,8 +2,8 @@ program loss_of_precision
   use, intrinsic :: iso_fortran_env, only : real64
   implicit none
   real, parameter :: pi = 3.141592653589793238462643383
-  real(real64), parameter :: pi_wrong = 3.141592653589793238462643383
-  real(real64), parameter :: pi_right = 3.141592653589793238462643383_real64
+  real(real64),parameter::pi_wrong = 3.141592653589793238462643383
+  real(real64),parameter::pi_right = 3.141592653589793238462643383_real64
   print*, "real variable, real literal     :", pi
   print*, "real64 variable, real literal   :", pi_wrong
   print*, "real64 variable, real64 literal :", pi_right
